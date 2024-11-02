472cd4d9289b9583e4f2ae7e9f568bf728160ad8
kh23hernandez <khernandezb6@miumg.edu.gt>
2024-11-02 04:11:49 -0600

    Add files via upload

---
+++ dist/frontend-login/server/main.server.mjs
@@ -0,0 +1,10 @@
+import './polyfills.server.mjs';
+import {
+  main_server_default
+} from "./chunk-OSNUGLUM.mjs";
+import "./chunk-524NZ4NL.mjs";
+import "./chunk-PTRYWQQD.mjs";
+export {
+  main_server_default as default
+};
+//# sourceMappingURL=main.server.mjs.map
